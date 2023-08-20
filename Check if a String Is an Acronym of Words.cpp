8cb9c86d2168694d95c53cbfb8834ee5a5467b1e
virgin-coder <87562272+virgin-coder@users.noreply.github.com>
2023-08-20 08:58:24 +0530

    Create Check if a String Is an Acronym of Words.cpp

---
+++ Check if a String Is an Acronym of Words.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isAcronym(vector<string>& words, string s) {
+        string t;
+        for(auto it : words) {
+            t += it[0];
+        }
+        return t == s;
+    }
+};
